3b660e01cf4abbbe76596373a2bb7192a7222049
duwei <1069646435@qq.com>
2021-02-27 15:32:50 +0800

    css file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 import x from './x.js'
-console.log('hi')
+console.log('x')
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');//添加
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');//添加
 const path = require('path');
+
 
 
 module.exports = {
@@ -7,11 +9,24 @@ module.exports = {
     entry: './src/index.js', //默认入口文件
     output: {//输出
       path: path.resolve(__dirname, 'dist'),//默认定位,文件的目标路径
-      filename: 'index.[contenthash].js', //。[name]可以改。
+      filename: 'index.[contenthash].js',
+  },
+  devServer: {
+    contentBase: './dist',
   },
   plugins: [new HtmlWebpackPlugin({
     title: 'woshisb',
     template: 'src/assets/index.html'
-  })],
+  })
+],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
-//总的意思是默认把src下的index.js转译后放到dist下的main.js
+
