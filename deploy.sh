558173d513ddecbc2274359040e159b95a6571d5
duwei <1069646435@qq.com>
2021-02-28 10:34:49 +0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@ yarn build &&
 git checkout gh-pages &&
 rm -rf *.html *.js *.css *.png &&
 mv dist/* ./ &&
-re -rf dist
+re -rf dist;
 git add . &&
 git commit -m 'update' &&
-git push
+git push &&
 git checkout -
